b2e5d4a92cb875492b08c1b387b3322adb8441cf
azka1415 <rafifzaydan4@gmail.com>
2022-06-06 09:50:49 +0700

    updated eslint

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const Hapi = require('@hapi/hapi');
+const port = 7000;
+const init = async () => {
+  // eslint-disable-next-line new-cap
+  const server = Hapi.Server({
+    port: port,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(routes);
+  await server.start();
+  console.log(`server running at localhost:${port}`);
+};
+
+init();
